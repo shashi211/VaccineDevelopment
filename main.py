50eb10aa4f63373442698c2259bd381e76c0c955
SHASHI PRAKASH <shashiprakash.srk@gmail.com>
2021-12-21 09:40:36 +0530

    adding gitignore

--- main.py
+++ main.py
@@ -82,6 +82,52 @@ class IMMUNIZATION:
                     visited.add(neighbour)
                     queue.append(neighbour)
 
+        # Use BFS to check path between s and d
+
+    def BFS_SP(self, start, goal):
+        explored = []
+
+        # Queue for traversing the
+        # graph in the BFS
+        queue = [[start]]
+
+        # If the desired node is
+        # reached
+        if start == goal:
+            print("Same Node")
+            return
+
+        # Loop to traverse the graph
+        # with the help of the queue
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+
+            # Condition to check if the
+            # current node is not visited
+            if node not in explored:
+                neighbours = self.adj_list[node]
+
+                # Loop to iterate over the
+                # neighbours of the node
+                for neighbour in neighbours:
+                    new_path = list(path)
+                    new_path.append(neighbour)
+                    queue.append(new_path)
+
+                    # Condition to check if the
+                    # neighbour node is the goal
+                    if neighbour == goal:
+                        print("Shortest path = ", *new_path)
+                        return
+                explored.append(node)
+
+        # Condition when the nodes
+        # are not connected
+        print("So sorry, but a connecting" \
+              "path doesn't exist :(")
+        return
+
 
 vaccineList = []  # list containing vaccine and strains
 edges = []  # matrix of edges/associations
@@ -93,4 +139,4 @@ graph.graph()
 graph.print_adj()
 graph.displayStrains("CoviShield")
 graph.displayVaccine("229E")
-graph.bfs("Covaxin")
+graph.BFS_SP("Covaxin","CoviShield")
